8fb9ad8c1feb5c2d220651594107cdcd4d096c6d
Miheel <mikaelleuf@gmail.com>
2019-11-12 00:40:42 +0000

    tweeked polybar and added rofi config

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -6,6 +6,6 @@ killall -q polybar
 #whit until the process have been shut down
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
-#polybar top &
-polybar exeple -r &
-polybar bottom &
+
+polybar top -r &
+polybar bottom -r &
